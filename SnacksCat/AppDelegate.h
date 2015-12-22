0da97cf49746b38e47ba4e0c024c62cf5ad2ce73
K.W <wj931122@163.com>
2015-12-23 05:20:04 +0800

    创建SnacksCat

---
+++ SnacksCat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SnacksCat
+//
+//  Created by 吴健 on 15/12/23.
+//  Copyright © 2015年 Kasim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SnacksCat/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SnacksCat
+//
+//  Created by 吴健 on 15/12/23.
+//  Copyright © 2015年 Kasim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
